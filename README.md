6e5cb4d774b30cd78f5665a80526cfd083b7b1b2
Yang Nan <nanyang@kth.se>
2021-01-06 19:12:01 +0200

    1.07

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # project
+
 DevOps with Kubernetes Part1 Project
--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
+
+app.use(morgan('tiny'));
 
 const PORT = 3001;
 
