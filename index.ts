e3cd120aae14ceba2c912b4e1429659742144856
Yang Nan <nanyang@kth.se>
2021-01-06 16:04:35 +0200

    1.05

--- index.ts
+++ index.ts
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = 3001;
 
+app.get('/', (_req, res) => {
+  res.send('Get /');
+});
+
 app.listen(PORT, () => {
   console.log(`Server started in port ${PORT}`);
 });
