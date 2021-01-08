4266cad86f1915e42dc7523f57838de676cbae79
Yang Nan <nanyang@kth.se>
2021-01-08 03:32:59 +0200

    1.12

--- index.ts
+++ index.ts
@@ -1,14 +1,31 @@
 import express from 'express';
 import morgan from 'morgan';
+import fs from 'fs';
+import axios from 'axios';
 
 const app = express();
 
 app.use(morgan('tiny'));
 
 const PORT = 3001;
+const PATH = '/usr/src/app/files/1200.jpg';
 
-app.get('/', (_req, res) => {
-  res.send('Get /');
+const fileExists = async () => {
+  try {
+    if (!fs.existsSync(PATH)) {
+      const response = await axios.get('https://picsum.photos/1200', { responseType: 'stream' });
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+      response.data.pipe(fs.createWriteStream(PATH));
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+  
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+app.get('/', async (_req, res) => {
+  await fileExists();
+  res.sendFile(PATH);
 });
 
 app.listen(PORT, () => {
