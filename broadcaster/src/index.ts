b8ce580686f1035b57830a150bc51681bfe217dc
Yang Nan <nanyang@kth.se>
2021-02-18 23:43:11 +0200

    4.06

--- broadcaster/src/index.ts
+++ broadcaster/src/index.ts
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import { NatsError, Msg } from 'nats';
 
-import config from './utils/config';
+import message from './utils/message';
 import nats from './utils/nats';
 
 const natsConnect = async () => {
@@ -13,22 +13,7 @@ const natsConnect = async () => {
 };
 void natsConnect();
 
-const TELEGRAM_URL = config.TELEGRAM_URL;
-const CHAT_ID = config.CHAT_ID;
-
-const sendMessage = async (message: string) => {
-  await axios.get(`${TELEGRAM_URL}/sendMessage?chat_id=${CHAT_ID}&parse_mode=HTML&text=${message}`);
-  console.log('Send message successfully!');
-};
-
-const object = {
-  title: 'TODO 1',
-  done: false,
-};
-
-const message = `<code>${JSON.stringify(object, null, '\t')}</code>`;
-
-const mes_title = '<b>Todos NATS broadcaster</b>';
-const mes_subtitle = 'A task was <i>created</i>: ';
-const mes_foot = 'broadcasted by <code>r</code> @ <code>p</code>';
-void sendMessage(`${mes_title}\n\n${mes_subtitle}\n\n${message}\n\n${mes_foot}`);
+nats.subscribe((err: NatsError | null, msg: Msg) => {
+  if (err) throw err;
+  message.sendMessage(msg);
+});
